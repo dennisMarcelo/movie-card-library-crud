b845cfabafd6e4f932dca8708c6e3edb90562ced
dennisMarcelo <dennismarcelo15@gmail.com>
2021-08-31 11:34:49 -0300

    Updates

---
+++ precache-manifest.98f1959bd88bcc93ba2cd539c3c0c123.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b91ad6146718262a53031459dc7d6d9",
+    "url": "%7BdennisMarcelo%7D.github.io/movie-card-library-crud/index.html"
+  },
+  {
+    "revision": "3c3a39a20503ef40210e",
+    "url": "%7BdennisMarcelo%7D.github.io/movie-card-library-crud/static/css/main.05c859dd.chunk.css"
+  },
+  {
+    "revision": "8dae88cf983209001e23",
+    "url": "%7BdennisMarcelo%7D.github.io/movie-card-library-crud/static/js/2.9aaca265.chunk.js"
+  },
+  {
+    "revision": "3c3a39a20503ef40210e",
+    "url": "%7BdennisMarcelo%7D.github.io/movie-card-library-crud/static/js/main.0e3304c5.chunk.js"
+  },
+  {
+    "revision": "e6e48620b2341f33661d",
+    "url": "%7BdennisMarcelo%7D.github.io/movie-card-library-crud/static/js/runtime-main.f98973fb.js"
+  }
+]);
